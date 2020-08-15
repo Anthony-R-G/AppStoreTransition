95116926af61e6f0bbdf0631640168888b985e96
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-14 22:27:13 -0400

    Created collectionview + layout

--- AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Controllers/ViewController.swift
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Controllers/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
+let reuseIdentifier = "cellId"
 class ViewController: UIViewController {
-
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 335, height: 410)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 30
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.dataSource = self
+        cv.delegate = self
+        cv.register(CardCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        return cv
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
     }
-
-
 }
 
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailVC = DetailViewController()
+        self.navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
