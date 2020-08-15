a25ca3c30f1495243b355ab20ec9213005d80ed9
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-14 22:38:46 -0400

    Added subviews and constraints to the cv cell

--- AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/CommonView.swift
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/CommonView.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class CommonView: UIView {
-
+    //MARK: -- UI Element Initialization
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var blurbLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var backgroundImage: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -18,4 +41,53 @@ class CommonView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+}
+
+
+//MARK: -- Add Subviews & Constraints
+
+fileprivate extension CommonView {
+    func addSubviews() {
+        let UIElements = [titleLabel, subtitleLabel, blurbLabel]
+        UIElements.forEach { addSubview($0) }
+    }
+    
+    func setConstraints() {
+        setTitleLabelConstraints()
+        setSubtitleLabelConstraints()
+        setBlurbLabelConstraints()
+    }
+    
+    func setTitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: rightAnchor)
+        ])
+    }
+    
+    func setSubtitleLabelConstraints() {
+        NSLayoutConstraint.activate([
+            subtitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            subtitleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16)
+        ])
+    }
+    
+    func setBlurbLabelConstraints() {
+        NSLayoutConstraint.activate([
+            blurbLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            blurbLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 16),
+            blurbLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 16)
+        ])
+    }
+    
+    func setBackgroundImageConstraints() {
+        NSLayoutConstraint.activate([
+            backgroundImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            backgroundImage.centerYAnchor.constraint(equalTo: centerYAnchor),
+            backgroundImage.heightAnchor.constraint(equalToConstant: 500),
+            backgroundImage.widthAnchor.constraint(equalToConstant: 500)
+        
+        ])
+    }
 }
