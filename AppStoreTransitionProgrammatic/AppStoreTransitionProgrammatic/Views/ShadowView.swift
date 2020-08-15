71a77ce27400c31ef9c0a1f3da1f2662a451034d
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-15 18:34:39 -0400

    Removed self references in shadow view

--- AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/ShadowView.swift
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/ShadowView.swift
@@ -12,18 +12,18 @@ import UIKit
 class ShadowView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.commonInit()
+        commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.commonInit()
+        commonInit()
     }
     
     func commonInit() {
-        self.layer.shadowRadius = 8
-        self.layer.shadowOffset = CGSize(width: 0, height: 8)
-        self.layer.shadowOpacity = 0.25
-        self.layer.masksToBounds = false
+        layer.shadowRadius = 8
+        layer.shadowOffset = CGSize(width: 0, height: 8)
+        layer.shadowOpacity = 0.25
+        layer.masksToBounds = false
     }
 }
