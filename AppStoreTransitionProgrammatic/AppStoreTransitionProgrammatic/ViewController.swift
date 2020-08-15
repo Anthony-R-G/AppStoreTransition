fb635478b8b9cd16811bf544eacde27981a6bf41
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-14 22:20:09 -0400

    Removed storyboard references

---
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreTransitionProgrammatic
+//
+//  Created by Anthony Gonzalez on 8/14/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+
+
+}
+
