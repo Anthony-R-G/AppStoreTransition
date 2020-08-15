b02fe637cf9476a58fdd8a03425ea761924733ba
Anthony-R_G <anthonygonzalez@pursuit.org>
2020-08-14 22:23:19 -0400

    Added empty controller/view files

---
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  AppStoreTransitionProgrammatic
+//
+//  Created by Anthony Gonzalez on 8/14/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    
+}
---
+++ AppStoreTransitionProgrammatic/AppStoreTransitionProgrammatic/Views/CommonView.swift
@@ -0,0 +1,21 @@
+//
+//  CommonView.swift
+//  AppStoreTransitionProgrammatic
+//
+//  Created by Anthony Gonzalez on 8/14/20.
+//  Copyright © 2020 Antnee. All rights reserved.
+//
+
+import UIKit
+
+class CommonView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
